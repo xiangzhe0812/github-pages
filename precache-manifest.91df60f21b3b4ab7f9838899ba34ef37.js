c9f098b3d4aae265750e86a945b9a6887df493c3
Alex Xiang <xiangzhe0812@gmail.com>
2020-10-06 15:39:54 +1000

    Updates

---
+++ precache-manifest.91df60f21b3b4ab7f9838899ba34ef37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb03e531fc3a81773e2ec75423bde48c",
+    "url": "/github-page/index.html"
+  },
+  {
+    "revision": "ba0744e896f3494cf657",
+    "url": "/github-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd414179c6e26f26a561",
+    "url": "/github-page/static/js/2.25a61a9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-page/static/js/2.25a61a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0744e896f3494cf657",
+    "url": "/github-page/static/js/main.9ef572ac.chunk.js"
+  },
+  {
+    "revision": "9b55972bb59066d8fb32",
+    "url": "/github-page/static/js/runtime-main.d6abdcb6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-pages/precache-manifest.fbc08dd2ea7c74718bd8cfef910e6953.js"
+  "/github-page/precache-manifest.91df60f21b3b4ab7f9838899ba34ef37.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
